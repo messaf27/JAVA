7a5d2b5d41acf20392b90385ec985136875e5d59
Egor <messaf@mail.ru>
2023-04-08 14:04:36 +1000

    update task05-02

--- src/Task05_02.java
+++ src/Task05_02.java
@@ -60,28 +60,60 @@ public class Task05_02 {
             mapRaiting.put(arrNames[i], random.nextInt(5));
         }
 
+        System.out.println("========== Задача №05-02 =============");
+
         // Сортируем по рейтинку (значению)
-        // mapRaiting.entrySet().stream()
-        // .sorted(Map.Entry.<String, Integer>comparingByValue()
-        // .reversed()).forEach(System.out::println);
+        System.out.println("--------------------------------------");
+        System.out.println("Вывод сортировка по возврастанию популярности:");
+        System.out.println("--------------------------------------");
+        mapRaiting.entrySet().stream()
+        .sorted(Map.Entry.<String, Integer>comparingByValue()
+        .reversed()).forEach(System.out::println);
 
         HashMap<String, Integer> mapRepPersonNames = new HashMap<>();
 
         for (Entry<String, Integer> person : mapRaiting.entrySet()) {
-            // System.out.println(person.getKey());
-            String[] nameArr =  person.getKey().toString().split(" ");
-            // System.out.println(nameArr[0]);
             int counter = 0;
-
+            String[] nameArr =  person.getKey().toString().split(" ");
             for (Entry<String, Integer> map : mapRaiting.entrySet()) {
                 if(map.getKey().toString().contains(nameArr[0]))
                 {
                     counter++;
-                    mapRepPersonNames.put(map.getKey(), counter);
+                    mapRepPersonNames.put(nameArr[0], counter);
                 }
             }
         }
-
-        System.out.println(mapRepPersonNames);
+        System.out.println("----------------------------------");
+        System.out.printf("Результат подсчета повторяющихся имён:\n%s\n", mapRepPersonNames);
+        System.out.println("==================================");
     }
 }
+
+/*
+========== Задача №05-02 =============
+--------------------------------------
+Вывод сортировка по возврастанию популярности:
+--------------------------------------
+Марина Лугова=4
+Иван Юрин=4
+Павел Чернов=4
+Петр Чернышов=4
+Марина Светлова=3
+Иван Иванов=3
+Анна Крутова=3
+Анна Мусина=1
+Мария Федорова=1
+Анна Владимирова=1
+Иван Ежов=1
+Светлана Петрова=0
+Иван Мечников=0
+Мария Савина=0
+Кристина Белова=0
+Петр Петин=0
+Петр Лыков=0
+Мария Рыкова=0
+----------------------------------
+Результат подсчета повторяющихся имён:
+{Мария=3, Марина=2, Светлана=1, Анна=3, Кристина=1, Иван=4, Петр=4, Павел=1}
+==================================
+*/
