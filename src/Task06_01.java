0ddcf198052b4217d95e634550836bd93ad7d061
Egor <messaf@mail.ru>
2023-04-18 22:43:55 +1000

    Update Task 06

--- src/Task06_01.java
+++ src/Task06_01.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Map.Entry;
 
 /**
  * NoteBook
@@ -7,14 +10,14 @@ import java.util.Random;
 class NoteBook {
 
     static private int idCounter = 0; // static object counter
-    
+
     private Integer uniqId;
     private Integer ram_gb;
     private Integer hdd_gb;
     private String os_name;
     private String color;
 
-    NoteBook(){
+    NoteBook() {
         idCounter++;
         uniqId = idCounter;
         this.hdd_gb = 500;
@@ -23,7 +26,7 @@ class NoteBook {
         this.color = "Black";
     }
 
-    NoteBook(Integer ram, Integer hdd, String os, String col){
+    NoteBook(Integer ram, Integer hdd, String os, String col) {
         this(); // for ++ id
         this.hdd_gb = hdd;
         this.ram_gb = ram;
@@ -31,10 +34,26 @@ class NoteBook {
         this.color = col;
     }
 
+    public Integer getHDDValue() {
+        return this.hdd_gb;
+    }
+
+    public Integer getRAMValue() {
+        return this.ram_gb;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getOSName() {
+        return this.os_name;
+    }
+
     @Override
-    public String toString(){
-        return String.format("Notebook [ID: %d, Color: %s, RAM: %d GB, HDD: %d GB, OS: %s] ", 
-                                                uniqId, this.color, this.ram_gb, this.hdd_gb, this.os_name);
+    public String toString() {
+        return String.format("[ID: %d, Color: %s, RAM: %d GB, HDD: %d GB, OS: %s]",
+                uniqId, this.color, this.ram_gb, this.hdd_gb, this.os_name);
     }
 }
 
@@ -44,53 +63,122 @@ class NoteBook {
 public class Task06_01 {
 
     public static String[] osNames = new String[] {
-        "Windows 7",
-        "Windows 10",
-        "Windows 11",
-        "MacOS",
-        "Ubuntu"    
+            "Windows 7",
+            "Windows 10",
+            "Windows 11",
+            "MacOS",
+            "Ubuntu"
     };
 
     public static String[] colors = new String[] {
-        "Dark Gray",
-        "Silver",
-        "White",
-        "Black",
-        "Dark Blue"    
+            "Dark Gray",
+            "Silver",
+            "White",
+            "Black",
+            "Dark Blue"
     };
 
+    public static HashMap<Integer, String> filterParametres = new HashMap<>();
+
     public static void main(String[] args) {
-        
-        HashSet<NoteBook> nbShop = CreateDataBase(200);
+
+        HashSet<NoteBook> nbShop = CreateDataBase(15);
         PrintShop(nbShop);
+
+        filterParametres.put(1, "ОЗУ");
+        filterParametres.put(2, "Объём ЖД");
+        filterParametres.put(3, "Операционная система");
+        filterParametres.put(4, "Цвет");
+
+        SetFilter(nbShop, filterParametres);
+
     }
 
-    public static HashSet<NoteBook> CreateDataBase(int num){
+    public static HashSet<NoteBook> CreateDataBase(int num) {
 
         HashSet<NoteBook> db = new HashSet<>();
 
         Random rnd = new Random();
 
         for (int i = 0; i < num; i++) {
-            db.add
-            (
-                new NoteBook
-                (
-                    rnd.nextInt(32), 
-                    rnd.nextInt(2000),
-                    osNames[rnd.nextInt(5)],
-                    colors[rnd.nextInt(5)]
-                )
-            );
+            db.add(
+                    new NoteBook(
+                            rnd.nextInt(32),
+                            rnd.nextInt(2000),
+                            osNames[rnd.nextInt(5)],
+                            colors[rnd.nextInt(5)]));
         }
         return db;
-    }  
+    }
 
-    public static void PrintShop(HashSet<NoteBook> set)
-    {      
+    public static void PrintShop(HashSet<NoteBook> set) {
         for (NoteBook noteBook : set) {
             System.out.println(noteBook);
         }
+    }
+
+    public static void SetFilter(HashSet<NoteBook> set, HashMap<Integer, String> filter) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Задайте критерии фильтра: ");
+        for (Entry<Integer, String> f : filter.entrySet()) {
+            System.out.println(String.format("%d - %s", f.getKey(), f.getValue()));
+        }
+        System.out.println("Введите число: ");
+        int inputParametrNum = scan.nextInt();
+
+        switch (inputParametrNum) {
+
+            case 1:
+                System.out.printf("Введите мин. параметр \"%s\": \n", filter.get(inputParametrNum));
+                int minRamVal = scan.nextInt();
+                for (NoteBook noteBook : set) {
+                    if (noteBook.getRAMValue() <= minRamVal) {
+                        System.out.println(noteBook);
+                    }
+                }
+                break;
+
+            case 2:
+                System.out.printf("Введите мин. параметр \"%s\": \n", filter.get(inputParametrNum));
+                int minHddVal = scan.nextInt();
+                for (NoteBook noteBook : set) {
+                    if (noteBook.getHDDValue() <= minHddVal) {
+                        System.out.println(noteBook);
+                    }
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < osNames.length; i++)
+                    System.out.printf("%d - %s\n", i + 1, osNames[i]);
+
+                System.out.printf("Задайте параметр \"%s\": \n", filter.get(inputParametrNum));
+                int osVal = scan.nextInt();
+                for (NoteBook noteBook : set) {
+                    if (noteBook.getOSName().contains(osNames[osVal - 1])) {
+                        System.out.println(noteBook);
+                    }
+                }
+                break;
+
+            case 4:
+                for (int i = 0; i < osNames.length; i++)
+                    System.out.printf("%d - %s\n", i + 1, colors[i]);
+
+                System.out.printf("Задайте параметр \"%s\": \n", filter.get(inputParametrNum));
+
+                int colVal = scan.nextInt();
+                for (NoteBook noteBook : set) {
+                    if (noteBook.getColor().contains(colors[colVal - 1])) {
+                        System.out.println(noteBook);
+                    }
+                }
+                break;
+        }
+
+        scan.close();
+
     }
 
 }
